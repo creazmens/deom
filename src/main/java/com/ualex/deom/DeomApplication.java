29b037797bdb3edbd47fedc02b8b8a3892e2239a
creazmens <18626235184@163.com>
2019-07-06 19:58:28 +0800

    demo test

---
+++ src/main/java/com/ualex/deom/DeomApplication.java
@@ -0,0 +1,13 @@
+package com.ualex.deom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeomApplication.class, args);
+    }
+
+}
